3ec9b63a1a0417529590afe53b2378ecf8c72cd7
wendy198622 <wendy198622@gmail.com>
2020-10-29 12:53:55 +0800

    home modify

--- js/main.js
+++ js/main.js
@@ -100,3 +100,29 @@
     $( ".datepicker_pop" ).datepicker();
 
 })(jQuery);
+
+$('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    }, 
+  
+    {
+  
+      duration: 2000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+  
+    });  
+    
+    
+  
+  });
